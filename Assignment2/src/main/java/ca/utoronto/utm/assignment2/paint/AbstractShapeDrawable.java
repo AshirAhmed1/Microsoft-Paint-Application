f84168719d7dd89f6911780af34592f10e5ac001
Alexander Phan <alexanderjacob.phan@mail.utoronto.ca>
2025-11-13 12:42:22 -0500

    updated javadoc

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/AbstractShapeDrawable.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/AbstractShapeDrawable.java
@@ -2,32 +2,74 @@ package ca.utoronto.utm.assignment2.paint;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+/**
+ * An abstract class that represents a drawable shape which can be added to the canvas in
+ * the paint application.
+ */
+
 public abstract class AbstractShapeDrawable implements Drawable {
+
     protected Color color;
     protected double thickness;
     protected boolean filled = true;
 
+    /**
+     * Construct a new AbstractShapeDrawable with initial default values
+     */
     public AbstractShapeDrawable() {}
 
+    /**
+     * Set the color of the shape
+     * @param color
+     */
     public void setColor(Color color) {
         this.color = color;
     }
 
+    /**
+     * Returns the current color of the shape.
+     *
+     * @return color of the shape
+     */
     public Color getColor() {return color;}
 
+    /**
+     * Sets the current thickness of the shapes outline
+     *
+     * @param thickness
+     */
     public void setThickness(double thickness) {
         this.thickness = thickness;
     }
 
+    /**
+     * Returns the thickness of the shape
+     *
+     * @return the current thickness
+     */
     public double getThickness() {return thickness;}
 
+    /**
+     * Returns whether the shape is filled.
+     *
+     * @return if the shape is filled
+     */
     public boolean isFilled() {
         return filled;
     }
 
+    /**
+     *
+     *
+     * @param filled
+     */
     public void setFilled(boolean filled) {
         this.filled = filled;
     }
 
+    /**
+     * Draws the specific shape onto the canvas.
+     * @param gc which is the current canvas.
+     */
     public abstract void draw(GraphicsContext gc);
 }
